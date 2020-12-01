4475b49b8b8bc0c7af5b794ab40c9a91498a9223
Maxico11 <49170767+Maxico11@users.noreply.github.com>
2020-12-01 12:37:56 -0500

    Update simulate_data.R

--- simulate_data.R
+++ simulate_data.R
@@ -3,7 +3,7 @@
 # ====================================================================
 
 set.seed(1234)
-nobs = 100
+nobs = 50
 nind = 100
 nlevels <- 4
 levels.cat <- rnorm(n = nlevels, mean = 0, sd = 2)
@@ -14,3 +14,6 @@ table.cat <- data.frame(categorique = 1:4, val = levels.cat)
 x1 <- runif(min = 0, max = 1, n = nobs*nind)
 x2 <- runif(min = 0, max = 1, n = nobs*nind)
 y <- 2 + x1*3 + x2*(-2) + 0.5*x1*x2 + rep(indbt, each = nobs) + table.cat$val[match(categorique, table.cat$categorique)] + rnorm(sd = 2, n = nobs*nind)
+
+# Create the simulated dataset
+data <- data.frame(cbind(id, environnement = categorique, x1, x2, y))
