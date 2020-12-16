239a666c664ad9a139af3decbe635df448de0ef0
Maxico11 <49170767+Maxico11@users.noreply.github.com>
2020-12-16 10:20:16 -0500

    Update simulate_data.R
    Augmented the 0.5 value to 12.5 in the formula to create "y" so that the interaction between x1 and x2 is high and thus visible in the plots

--- simulate_data.R
+++ simulate_data.R
@@ -13,7 +13,7 @@ categorique <- sample(x = 1:4, size = nobs*nind, replace = TRUE)
 table.cat <- data.frame(categorique = 1:4, val = levels.cat)
 x1 <- runif(min = 0, max = 1, n = nobs*nind)
 x2 <- runif(min = 0, max = 1, n = nobs*nind)
-y <- 2 + x1*3 + x2*(-2) + 0.5*x1*x2 + rep(indbt, each = nobs) + table.cat$val[match(categorique, table.cat$categorique)] + rnorm(sd = 2, n = nobs*nind)
+y <- 2 + x1*3 + x2*(-2) + 12.5*x1*x2 + rep(indbt, each = nobs) + table.cat$val[match(categorique, table.cat$categorique)] + rnorm(sd = 2, n = nobs*nind)
 
 # Create the simulated dataset
 data <- data.frame(cbind(id, environnement = categorique, x1, x2, y))
