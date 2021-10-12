be40aa834852e21f63bb7432bc706cad605db8bd
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2021-10-12 10:57:27 -0400

    Update estimation_variance_BLUP_R_R2.R
    cleaned the code and added some comments

--- estimation_variance_BLUP_R_R2.R
+++ estimation_variance_BLUP_R_R2.R
@@ -1,5 +1,13 @@
 # ====================================================================
-# Extracte the BLUP (Best Linear Unbiaised Predictor)
+
+#       Extracte the BLUP (Best Linear Unbiaised Predictor)
+
+# ====================================================================
+
+
+
+# ====================================================================
+# 1. Importer les données et rouler le modèle
 # ====================================================================
 
 # Verifier que la dataset simulee
@@ -8,54 +16,85 @@ head(data)
 # Loading package 
 library(lme4)
 
-# Creer un model mixte
-model<-lmer(y~T1+T2+T1:T2+block+(1|id), data=data2)
-summary(model) 
+# Creer un modèle mixte
+model <- lmer(y~ T1 + T2 + T1:T2 + block+ (1 | id), data = data2)
+summary(model)
 
 # Extraire les BLUPS utilisant la fonction ranef
-BLUPS<-ranef(model)$id
+BLUPS <- ranef(model)$id
 
 # Check the BLUPS
 nrow(BLUPS)
 
+# *** À noter: n'oubliez pas de vérifier que votre modèle est correct ***
+# ex. vérifier l'homogénéité des résidus, la variance entre les groupes, etc...
+
+# ====================================================================
+# ====================================================================
+
+
+
 
 
 # ====================================================================
-# Estimation du R2 du model via MuMIn 
+# 2. Estimation du R2 du modèle via MuMIn 
 # ====================================================================
+
+# À noter, cette méthode ne fonctionne pas avec toutes les familles
+# de distributions.
 
 # Loading packages
 library(MuMIn)
 
-# Estimation du R2 du model 
+# Estimation du R2 du modèle
 r.squaredGLMM(model)
 
+# ====================================================================
+# ====================================================================
+
+
+
 
 
 # ====================================================================
-# Estimation de la Repetabilite via lm4 
+# 3. Estimation de la Repetabilite via lme4 
 # ====================================================================
 
-# Extraire les estimes de variance et residus
+# Extraire les estimés de variance et résidus
 vc = VarCorr(model)
 residual_var = attr(vc,'sc')^2
 intercept_var = attr(vc$id,'stddev')[1]^2
 
 # Calculer la repetabilite R
-R_lm4 = intercept_var/(intercept_var+residual_var)
-R_lm4
+R_lme4 = intercept_var / (intercept_var + residual_var)
+R_lme4
+
+# ====================================================================
+# ====================================================================
+
+
 
 
 
 # ====================================================================
-# Estimation de la Repetabilite via RptR 
+# 4. Estimation de la Repetabilite via RptR 
 # ====================================================================
+
+# À noter, risque de ne pas fonctionner avec toutes les familles de 
+# distributions.
+
+# Ici, c'est une méthode par simulation
 
 # Loading packages
 library(rptR)
 
 # Estimation de la repetabilite et de son incertitude 
 
-R_rptR <- rpt(y ~ T1+T2+T1:T2+block+(1|id), grname = "id", data = data2, 
-    datatype = "Gaussian", nboot = 1000, npermut = 0)
+R_rptR <- rpt(y ~ T1 + T2 + T1:T2 + block + (1 | id), 
+              grname = "id", data = data2, 
+              datatype = "Gaussian",
+              nboot = 1000, npermut = 0)
 R_rptR
+
+# ====================================================================
+# ====================================================================
