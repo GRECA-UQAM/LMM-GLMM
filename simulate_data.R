f794bb201d5460ec6fdc329703313df342fb22de
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2021-10-12 11:18:08 -0400

    Update simulate_data.R
    cleaned the code

--- simulate_data.R
+++ simulate_data.R
@@ -1,72 +1,105 @@
-# ====================================================================
-# Simulate data to use in subsequent scripts and mixed model analyses
-# ====================================================================
+# ===================================================================================
+
+#        Simulate data to use in subsequent scripts for mixed model analyses
+#           Simuler des données pour les utiliser dans les modèles mixtes
+
+# ===================================================================================
+
+
+
+# =====================================================================
+# 1. Preparer les simulations / prepare the simulations
+# =====================================================================
 
 set.seed(1234)
 
-# Creation du nombre d individu et du nombre de mesure repetee.
+# Creation du nombre d'individus et du nombre de mesure repetees.
+# Create n individuals with n observations
 nobs = 50
 nind = 100
 
-# Creation du nombre d individu et du nombre de mesure repetee.
 
+# Creation du nombre d'individus et du nombre de mesures repetees. ***(MFF: WHY IS THIS HERE TWO TIMES?)***
 nlevels <- 4
 levels.cat <- rnorm(n = nlevels, mean = 0, sd = 2)
 
 indbt <- rnorm(n = nind, mean = 0, sd = 2)
 id <- rep(1:nind, each = nobs)
 
-# Creation du categorical variable
 
+# Creation d'une variable categorique
+# Create categorical variable
 categorique <- sample(x = 1:4, size = nobs*nind, replace = TRUE)
 
 table.cat <- data.frame(categorique = 1:4, val = levels.cat)
 
-#creation of  x1 and x2, two variable with min of 0, max of 1 and variable values
+
+# Creation de x1 et x2, des variables categoriques avec un min = 0 et max = 1
+# Creation of x1 and x2, two variable with min of 0, max of 1 and variable values
 x1 <- runif(min = 0, max = 1, n = nobs*nind)
 x2 <- runif(min = 0, max = 1, n = nobs*nind)
 
 
-# Creation of the response variable y via equation
-    # here intercept, 
-        #coef of partial regression of x1, x2 and its interactions, 
-        #random effect, 
-        #categorical variable
-        #residual
-y <- 2 + x1*3 + x2*(-2) + 12.5*x1*x2 + rep(indbt, each = nobs) + table.cat$val[match(categorique, table.cat$categorique)] + rnorm(sd = 2, n = nobs*nind)
+# Creation of the response variable y using the model equation
+   # here intercept, 
+       # coef of partial regression coefficient of x1, x2 and their interaction, 
+       # random effect, 
+       # categorical variable
+       # residual
 
+y <- 2 + x1*3 + x2*(-2) + 12.5*x1*x2 + 
+     rep(indbt, each = nobs) + 
+     table.cat$val[match(categorique, table.cat$categorique)] +
+     rnorm(sd = 2, n = nobs*nind)
+
+# =====================================================================
+# =====================================================================
+
+
+
+
+
+# =====================================================================
+# 2. Créer le jeu de données simulé / create the simulated dataset
+# =====================================================================
 
 # Create the simulated dataset
 data <- data.frame(cbind(id, environnement = categorique, x1, x2, y))
+
 
 # Check the dataset
 head(data)
 summary(data)
 
-# Check the dataset via plots
-par(mfrow=c(2,2))
-plot(y~x1, data)
-plot(y~x2, data)
-plot(y~environnement, data)
-plot(y~id, data)
+
+# Check the dataset with plots
+par(mfrow = c(2, 2))
+plot(y ~ x1, data)
+plot(y ~ x2, data)
+plot(y ~ environnement, data)
+plot(y ~ id, data)
 
 
 # Check the dataset via model
-model<-lm(y~x1+x2+x1:x2+environnement, data=data)
-summary(model) 
-#
+
+# Simple linear model
+model1 <- lm(y ~ x1 + x2 + x1:x2 + environnement, data = data)
+summary(model1) 
+
+# Mixed model
 library(lme4)
-model<-lmer(y~x1+x2+x1:x2+environnement+(1|id), data=data)
-summary(model) 
+model2 <- lmer(y ~ x1 + x2 + x1:x2 + environnement + (1 | id), data = data)
+summary(model2) 
 
-
+# ====================================================================
+# ====================================================================
 
 
 
 ######################################################################
 # ====================================================================
 # Other way to simulate a data with specific covariance between variables 
-# but not repeted measurement 
+# but not repeated measurements
 # ====================================================================
 
 # Loading package 
